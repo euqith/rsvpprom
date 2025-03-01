f7added6209effc83ceab1bf075ca943dd35a13c
Christian <christian.eyd@gmail.com>
2025-03-01 23:04:40 +0700

    commit-fixing-template

---
+++ resources/views/reserve.blade.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en" class="h-100 w-100">
+
+<head>
+    <title>THE LIVINGSTONES</title>
+
+    <link rel="preload" href="css/style.min.css" as="style">
+    <link rel="preload" href="js/script.min.js" as="script">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Required CSS files -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="img/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="img/favicon.ico">
+</head>
+
+<body id="page-top" class="h-100 w-100">
+
+    <!-- Preloader Start -->
+    <div class="preloader bg-white h-100 position-fixed w-100">
+        <svg width="100px" height="100px" viewBox="0 0 100 100" y="0px" x="0px" class="position-absolute start-50 top-50 translate-middle">
+            <g>
+                <path class="path-heart" d="M89.49,37.8c0,25.54-39.59,46.24-39.59,46.24S10.31,63.34,10.31,37.8c0-29.59,39.59-28.67,39.59,0C49.9,10.06,89.49,8.21,89.49,37.8 z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="3.5" stroke="#000000" fill="#e8ede6"></path>
+                <path class="path-heart-sm" d="M89.63,70.9c-3.57,10.15-22.21,12.84-22.21,12.84s-12.84-13.77-9.26-23.92c4.14-11.76,19.75-5.86,15.74,5.54 C77.78,54.33,93.78,59.13,89.63,70.9z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="3.5" stroke="#000000" fill="#6d8a91"></path>
+            </g>
+        </svg>
+    </div>
+    <!-- Preloader End -->
+
+
+    <!-- Header Start -->
+    <header class="fixed-top">
+        <nav class="navbar navbar-expand-lg p-0">
+            <div class="container">
+                <!-- Navbar brand (mobile) -->
+                <div class="navbar-brand align-items-center d-flex d-lg-none me-0">
+                    <img width="300" height="67" src="img/navbar-brand.png" alt="Logo">
+                </div>
+
+                <!-- Navbar toggler -->
+                <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Navbar menu -->
+                <div id="navbar" class="collapse navbar-collapse justify-content-lg-center">
+                    
+
+                    <!-- Navbar brand (desktop) -->
+                    <div class="navbar-brand d-none d-lg-block mx-3 mx-xl-5 text-center">
+                        <img width="300" height="67" src="img/navbar-brand.png" alt="Logo">
+                    </div>
+
+                    
+                </div>
+                <!-- //.collapse -->
+            </div>
+            <!-- //.container -->
+        </nav>
+    </header>
+    <!-- Header End -->
+
+
+    <!-- Spacer -->
+    <span class="spacer d-block w-100"></span>
+
+
+    <!-- Section - RSVP Start -->
+    <section id="rsvp">
+        <div class="container">
+            <div class="row align-items-center gx-lg-5 justify-content-center">
+                {{-- <div class="col-md-8 col-lg-5 mb-5 mb-lg-0">
+                    <div class="img-clip-path clip-love-story position-relative">
+                        <img src="images/rsvp.jpg" alt="" class="img-fluid">
+                    </div>
+                    <!-- //.img-clip-path -->
+                </div> --}}
+                <!-- //.col-md-8 -->
+
+                <div class="col-lg-7">
+
+                        <div class="row mb-3 mb-lg-4">
+                            <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSc8ZB_kV_tMjpOIatk6Ef1ofFHVBVsnxwOI95UpEyO3-v9cgQ/viewform?embedded=true" width="350" height="922" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                          
+                        </div>
+                       
+                </div>
+                <!-- //.col-lg-7 -->
+            </div>
+            <!-- //.row -->
+        </div>
+        <!-- //.container -->
+    </section>
+    <!-- Section - RSVP End -->
+
+
+    <!-- Footer Start -->
+    <footer class="bg-img-cover bg-overlay position-relative">
+        <div class="container position-relative">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2 class="font-alt fs-3 fw-bold text-uppercase text-light">We Can't Wait to See You!</h2>
+                    <p class="font-alt fs-5 fst-italic text-light">December, 17th 2023 | St. Patrick's Church, Washington, DC</p>
+                    <img width="100" src="img/ornament-divider.png" alt="" class="d-block mx-auto">
+                </div>
+                <!-- //.col-12 -->
+            </div>
+            <!-- //.row -->
+        </div>
+        <!-- //.container -->
+
+        <!-- Ornament -->
+        <span class="ornament-corner ornament-lg ornament-light"></span>
+    </footer>
+    <!-- Footer End -->
+
+
+    <!-- Required JS files -->
+    <script src="js/script.min.js"></script>
+</body>
+
+</html>
